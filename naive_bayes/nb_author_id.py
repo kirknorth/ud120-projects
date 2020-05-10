efa0e84301feb51610d0bc5dc610fbf8345331f2
Kirk North <kirk.w.north@gmail.com>
2020-05-10 16:41:14 -0400

    Add code for Lesson 1 (Naive Bayes)

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,6 +12,8 @@
     
 import sys
 from time import time
+from sklearn.naive_bayes import GaussianNB
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -25,9 +27,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
 
+# create Naive Bayes classifier
+clf = GaussianNB(priors=None)
+
+# train the classifier using training data
+t0 = time()
+clf.fit(features_train, labels_train)
+print('training time: {} s'.format(round(time()-t0, 3)))
+
+# predict labels for test data
+t0 = time()
+labels_predict = clf.predict(features_test)
+print('prediction time: {} s'.format(round(time()-t0, 3)))
+
+# get the mean accuracy of the classifier based on test data
+score = clf.score(features_test, labels_test)
+print('mean accuracy: {:.3f}'.format(score))
 
 #########################################################
-
-
